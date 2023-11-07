3cebb3b05079ad415f6e930c5ef65eb19b92e6d4
pronoyNath <pronoynath890@gmail.com>
2023-11-07 14:09:07 +0600

    booking list get

--- src/Pages/MyBookings/MyBookings.jsx
+++ src/Pages/MyBookings/MyBookings.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const MyBookings = () => {
+    const [bookingList, setBookingList] = useState();
+    const {user} = useContext(AuthContext);
+
+    const url = `http://localhost:5000/mybookings?email=${user?.email}`;
+    useEffect(() => {
+      axios.get(url,{withCredentials: true})
+      .then(res=>setBookingList(res.data))
+      .catch(err=>console.log(err))
+    }, [url]);
+    console.log(bookingList);
+
     return (
         <div>
             my bookings
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -63,7 +63,7 @@ const AuthProvider = ({ children }) => {
             if (currentUser) {
                 axios.post('http://localhost:5000/jwt', loggedUser, { withCredentials: true })
                     .then(res => {
-                        console.log(res.data);
+                        // console.log(res.data);
                     })
             }
             else {
